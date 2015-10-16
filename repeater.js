0c516ba6a6e2087c689e9413c438abadf4cdd228
nickfallon <nick.fallon@centralfax.co.uk>
2015-10-16 19:16:53 +0100

    Update repeater.js

--- repeater.js
+++ repeater.js
@@ -5,9 +5,14 @@
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+//Quick Use:
+
+//var x = new Repeater("your_container_id", some_array_of_objects);
+//x.render();
+
+
 //id = any DOM id acting as a container
 //jsonarray = any object array
-
 function Repeater(id, jsonarray) {
     this.root = document.getElementById(id);
     this.jsonarray = jsonarray;
